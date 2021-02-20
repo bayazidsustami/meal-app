d18f23467f6d7af2ff582bbc12126aea704d47fa
bayazid_sustami <bayazidsustamy@gmail.com>
2021-02-21 00:06:06 +0800

    update gradle

--- app/src/test/java/com/example/myapplication/ui/viewModels/MainViewModelTest.kt
+++ app/src/test/java/com/example/myapplication/ui/viewModels/MainViewModelTest.kt
@@ -1,39 +1,45 @@
 package com.example.myapplication.ui.viewModels
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import com.example.myapplication.apiConfig.ApiService
+import com.example.myapplication.data.models.ResponseMealsCategory
 import com.example.myapplication.data.repositories.categories.CategoriesImpl
 import com.example.myapplication.data.repositories.categories.CategoryRepository
 import com.example.myapplication.ui.viewModels.main.MainViewModel
+import com.example.myapplication.utils.Resource
+import com.example.myapplication.utils.State
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.mockk.spyk
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
 
 class MainViewModelTest {
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    @Mock
-    lateinit var apiService: ApiService
+    private val apiService: ApiService = mockk()
 
-    @Mock
-    lateinit var repository: CategoryRepository
-
-    lateinit var categoryImpl: CategoriesImpl
-
-    lateinit var viewModel: MainViewModel
-
+    private var useCase: CategoriesImpl = mockk()
+    private var mainRepo: CategoryRepository = mockk()
+    private val viewModel = MainViewModel(mainRepo)
+    private val observer: Observer<Resource<ResponseMealsCategory>> = spyk()
 
     @Before
     fun setup(){
-        MockitoAnnotations.initMocks(this)
-        viewModel = MainViewModel(repository)
+        viewModel.states.observeForever(observer)
     }
 
     @Test
     fun getCategoryTest(){
+
+
     }
+
+
 }
